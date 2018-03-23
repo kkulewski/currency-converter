ebe4737b351ef86e931b5ef879caf54098e973f4
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-23 02:42:06 +0100

    Add convert VM + bind entries in convert view

---
+++ CConv/CConv/CConv/ViewModels/ConvertViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CConv.ViewModels
+{
+    public class ConvertViewModel : BaseViewModel
+    {
+        private decimal _source;
+
+        private decimal _target;
+
+        public ConvertViewModel()
+        {
+            Source = 5.00M;
+            Target = 10.00M;
+        }
+
+        public decimal Source
+        {
+            get => _source;
+
+            set
+            {
+                _source = value;
+                OnPropertyChanged(nameof(Source));
+            }
+        }
+
+        public decimal Target
+        {
+            get => _target;
+
+            set
+            {
+                _target = value;
+                OnPropertyChanged(nameof(Target));
+            }
+        }
+    }
+}
--- CConv/CConv/CConv/Views/ConvertPage.xaml.cs
+++ CConv/CConv/CConv/Views/ConvertPage.xaml.cs
@@ -1,14 +1,18 @@
-﻿using Xamarin.Forms;
+﻿using CConv.ViewModels;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CConv.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ConvertPage : ContentPage
-	{
-		public ConvertPage()
-		{
-			InitializeComponent();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConvertPage : ContentPage
+    {
+        private ConvertViewModel _viewModel;
+
+        public ConvertPage()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new ConvertViewModel();
+        }
+    }
 }
