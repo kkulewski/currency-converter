62b4c8c85cc48098cfdb72df1e673603bbe173ca
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-23 02:54:16 +0100

    Add currency entry validation behavior

---
+++ CConv/CConv/CConv/Behaviors/Validators/CurrencyValidationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace CConv.Behaviors.Validators
+{
+    internal class CurrencyValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnBindableTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnBindableTextChanged;
+        }
+
+        private static void OnBindableTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!(sender is Entry entry))
+                return;
+
+            var isDecimal = decimal.TryParse(e.NewTextValue, out var number);
+            var isPositive = number > 0;
+
+            entry.BackgroundColor = isDecimal && isPositive ? Color.PaleGreen : Color.IndianRed;
+        }
+    }
+}
