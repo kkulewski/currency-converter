c7c8eb1b9d1aec0142bc378912135e1c2e4dd8ab
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-23 02:29:19 +0100

    Move main page to views dir

--- CConv/CConv/CConv/App.xaml.cs
+++ CConv/CConv/CConv/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace CConv
 {
@@ -13,7 +8,7 @@ namespace CConv
 		{
 			InitializeComponent();
 
-			MainPage = new CConv.MainPage();
+			MainPage = new Views.MainPage();
 		}
 
 		protected override void OnStart ()
