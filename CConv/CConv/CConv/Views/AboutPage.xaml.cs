c4246a06ee5d41bfafc4033c7a588dd1f5866a02
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-23 02:31:24 +0100

    Add about, convert pages

---
+++ CConv/CConv/CConv/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CConv.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
