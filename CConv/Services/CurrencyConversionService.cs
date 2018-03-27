a491e00ae80f70ef4915e2aea34a1eebea5dcfe1
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-27 23:12:40 +0200

    Add currency conversion service

---
+++ CConv/Services/CurrencyConversionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CConv.Models;
+
+namespace CConv.Services
+{
+    public class CurrencyConverterService : ICurrencyConverterService
+    {
+        public decimal Convert(ICurrency sourceCurrency, ICurrency targetCurrency, decimal value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CConv/Services/ICurrencyConversionService.cs
@@ -0,0 +1,9 @@
+﻿using CConv.Models;
+
+namespace CConv.Services
+{
+    public interface ICurrencyConversionService
+    {
+        decimal Convert(ICurrency sourceCurrency, ICurrency targetCurrency, decimal value);
+    }
+}
