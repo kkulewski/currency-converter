b0d8c66dbb0cf1383d4f3c21b4e8a4f9ffc3c375
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-14 02:48:47 +0200

    Add simple cache impl

---
+++ CConv/Services/Cache/ICache.cs
@@ -0,0 +1,9 @@
+﻿namespace CConv.Services.Cache
+{
+    public interface ICache<T> where T: class
+    {
+        void Set(T item);
+        T Get();
+        bool Valid();
+    }
+}
---
+++ CConv/Services/Cache/SimpleCache.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CConv.Services.Cache
+{
+    internal class SimpleCache<T> : ICache<T> where T: class
+    {
+        private T _item;
+        private DateTime _updatedOn;
+        private TimeSpan _lifetime;
+
+        public SimpleCache(TimeSpan lifetime)
+        {
+            _lifetime = lifetime;
+        }
+
+        public void Set(T item, TimeSpan lifetime)
+        {
+            _lifetime = lifetime;
+            Set(item);
+        }
+
+        public void Set(T item)
+        {
+            _item = item;
+            _updatedOn = DateTime.Now;
+        }
+
+        public T Get()
+        {
+            return _item;
+        }
+
+        public bool Valid()
+        {
+            return DateTime.Now - _updatedOn >= _lifetime;
+        }
+    }
+}
