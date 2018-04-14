ebbdeb6f5833e31da1de760293b5889c2adfdb3b
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-14 03:22:29 +0200

    Add update on property to cache

--- CConv/Services/Cache/ICache.cs
+++ CConv/Services/Cache/ICache.cs
@@ -1,10 +1,13 @@
-﻿namespace CConv.Services.Cache
+﻿using System;
+
+namespace CConv.Services.Cache
 {
     public interface ICache<T> where T: class
     {
-        bool Expired { get; }
-        void Expire();
         T Get();
         void Set(T item);
+        DateTime UpdatedOn { get; }
+        bool Expired { get; }
+        void Expire();
     }
 }
--- CConv/Services/Cache/SimpleCache.cs
+++ CConv/Services/Cache/SimpleCache.cs
@@ -5,19 +5,11 @@ namespace CConv.Services.Cache
     internal class SimpleCache<T> : ICache<T> where T: class
     {
         private T _item;
-        private DateTime _updatedOn;
         private TimeSpan _lifetime;
 
         public SimpleCache(TimeSpan lifetime)
         {
             _lifetime = lifetime;
-        }
-
-        public bool Expired => DateTime.Now - _updatedOn >= _lifetime;
-
-        public void Expire()
-        {
-            _updatedOn = DateTime.MinValue;
         }
 
         public T Get()
@@ -34,7 +26,16 @@ namespace CConv.Services.Cache
         public void Set(T item)
         {
             _item = item;
-            _updatedOn = DateTime.Now;
+            UpdatedOn = DateTime.Now;
+        }
+
+        public DateTime UpdatedOn { get; private set; }
+
+        public bool Expired => DateTime.Now - UpdatedOn >= _lifetime;
+
+        public void Expire()
+        {
+            UpdatedOn = DateTime.MinValue;
         }
     }
 }
