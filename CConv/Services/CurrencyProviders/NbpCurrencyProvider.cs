67a6bee1478e283689f29e93242a6b5a90a20364
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-14 02:33:00 +0200

    Refactor NBP currency provider

--- CConv/Services/CurrencyProviders/NbpCurrencyProvider.cs
+++ CConv/Services/CurrencyProviders/NbpCurrencyProvider.cs
@@ -7,11 +7,6 @@ using Newtonsoft.Json;
 
 namespace CConv.Services.CurrencyProviders
 {
-    public enum NbpTable
-    {
-        A,
-        B
-    }
 
     public class NbpCurrencyProvider : ICurrencyProvider
     {
@@ -33,18 +28,8 @@ namespace CConv.Services.CurrencyProviders
             try
             {
                 var uri = string.Format("http://api.nbp.pl/api/exchangerates/tables/{0}?format=json", _table);
-                var client = new WebClient();
-                var json = await client.DownloadStringTaskAsync(uri);
-
-                var deserialized = JsonConvert.DeserializeObject<ExchangeTable[]>(json);
-                var rates = deserialized[0].Rates;
-
-                Currencies = rates.Select(x => new Currency
-                {
-                    Code = x.Code,
-                    Name = x.Currency,
-                    Rate = decimal.Parse(x.Mid)
-                }).ToList<ICurrency>();
+                var json = await DownloadJson(uri);
+                Currencies = DeserializeCurrencyList(json);
 
                 return true;
             }
@@ -52,6 +37,32 @@ namespace CConv.Services.CurrencyProviders
             {
                 return false;
             }
+        }
+
+        private static async Task<string> DownloadJson(string uri)
+        {
+            string json;
+            using (var client = new WebClient())
+            {
+                json = await client.DownloadStringTaskAsync(uri);
+            }
+
+            return json;
+        }
+
+        private static IList<ICurrency> DeserializeCurrencyList(string json)
+        {
+            var deserialized = JsonConvert.DeserializeObject<ExchangeTable[]>(json);
+            var rates = deserialized[0].Rates;
+
+            var currencies = rates.Select(x => new Currency
+            {
+                Code = x.Code,
+                Name = x.Currency,
+                Rate = decimal.Parse(x.Mid)
+            });
+
+            return currencies.ToList<ICurrency>();
         }
 
         #region JSON
@@ -72,5 +83,11 @@ namespace CConv.Services.CurrencyProviders
         }
 
         #endregion
+    }
+
+    public enum NbpTable
+    {
+        A,
+        B
     }
 }
