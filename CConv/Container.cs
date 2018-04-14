80069292a123b86a5b5b0d17e95d16432e0bd66d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-14 04:46:30 +0200

    Add simple service locator

---
+++ CConv/Container.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CConv.Services.Conversion;
+using CConv.Services.CurrencyProviders;
+using CConv.ViewModels;
+
+namespace CConv
+{
+    /// <summary>
+    /// Extremely naive service locator / container.
+    /// </summary>
+    public class Container
+    {
+        private static ICurrencyConversionService Conversion { get; }
+        private static IList<ICurrencyProvider> Providers { get; }
+        private static ConvertViewModel ConvertViewModel { get; }
+
+
+        static Container()
+        {
+            Conversion = new CurrencyConversionService();
+            Providers = new List<ICurrencyProvider>
+            {
+                new FakeCurrencyProvider(),
+                new NbpCurrencyProvider(NbpTable.A),
+                new NbpCurrencyProvider(NbpTable.B)
+            };
+
+            ConvertViewModel = new ConvertViewModel(Conversion, Providers);
+        }
+
+        public static T Resolve<T>() where T : class
+        {
+            var t = typeof(T);
+
+            if (t.IsAssignableFrom(typeof(ConvertViewModel)))
+                return ConvertViewModel as T;
+
+            if (t.IsAssignableFrom(typeof(ICurrencyConversionService)))
+                return Conversion as T;
+
+            if (t.IsAssignableFrom(typeof(ICurrencyProvider)))
+                return Providers.FirstOrDefault() as T;
+
+            if (t.IsAssignableFrom(typeof(IList<ICurrencyProvider>)))
+                return Providers as T;
+
+            return null;
+        }
+    }
+}
--- CConv/Views/ConvertPage.xaml.cs
+++ CConv/Views/ConvertPage.xaml.cs
@@ -7,12 +7,10 @@ namespace CConv.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConvertPage : ContentPage
     {
-        private ConvertViewModel _viewModel;
-
         public ConvertPage()
         {
             InitializeComponent();
-            BindingContext = _viewModel = new ConvertViewModel();
+            BindingContext = Container.Resolve<ConvertViewModel>();
         }
     }
 }
