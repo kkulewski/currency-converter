cbbc2bb4a60b3eed34a095b643bf6f83a1b58ccf
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-15 22:51:04 +0200

    Add file writer droid impl

---
+++ CConv.Android/Services/FileWriter.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using CConv.Droid.Services;
+using CConv.Services.Storage;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileWriter))]
+namespace CConv.Droid.Services
+{
+    internal class FileWriter : IFileWriter
+    {
+        public async Task Write(string fileName, string data)
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var filePath = Path.Combine(documentsPath, fileName);
+            await Task.Run(() => File.WriteAllText(filePath, data));
+        }
+
+        public async Task<string> Read(string fileName)
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var filePath = Path.Combine(documentsPath, fileName);
+            return await Task.Run(() => File.ReadAllText(filePath));
+        }
+    }
+}
