56f2f78216d17735a6ee75d6c97b3e624ac71577
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-15 22:50:50 +0200

    Add file writer dependency service interface

---
+++ CConv/Services/Storage/IFileWriter.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace CConv.Services.Storage
+{
+    public interface IFileWriter
+    {
+        Task Write(string fileName, string data);
+        Task<string> Read(string fileName);
+    }
+}
