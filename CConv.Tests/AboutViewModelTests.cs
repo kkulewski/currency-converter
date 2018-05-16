b10f86df48f6dd109f0437e814bc7770bcba45ae
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-16 02:21:51 +0200

    Ensure aboutVM props are initialized properly

---
+++ CConv.Tests/AboutViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CConv.ViewModels;
+using Xunit;
+
+namespace CConv.Tests
+{
+    public class AboutViewModelTests
+    {
+        [Fact]
+        public void NewInstance_HasExpectedHomeUrl()
+        {
+            // Arrange
+            const string url = "test.com";
+            var vm = new AboutViewModel(url, string.Empty);
+
+            // Act & Assert
+            Assert.Equal(url, vm.HomeUrl);
+        }
+
+        [Fact]
+        public void NewInstance_HasExpectedGithubUrl()
+        {
+            // Arrange
+            const string url = "github.com/test";
+            var vm = new AboutViewModel(string.Empty, url);
+
+            // Act & Assert
+            Assert.Equal(url, vm.GitHubUrl);
+        }
+    }
+}
